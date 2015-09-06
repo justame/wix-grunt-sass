d7404ed9e63b2d93a9441a9680cb603c07d25db8
Yaron Nachshon <yaronn@wix.com>
2015-09-06 17:35:50 +0300

    init

---
+++ tasks/wix_grunt_sass.js
@@ -0,0 +1,49 @@
+/*
+ * wix-grunt-sass
+ *
+ *
+ * Copyright (c) 2015 Yaron Nachshon
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+  grunt.registerTask('wixGruntSass', 'compile scss/sass files into css', function () {
+    var template = require('es6-template-strings');
+    var fs = require('fs');
+    var options = this.options();
+    var importPathCmd = '';
+    var cmd, exec, done;
+    var nodeSass = require('node-sass');
+
+    exec = require('child_process').exec;
+
+    options.importPath.forEach(function (path) {
+      importPathCmd += '--include-path ' + path + ' ';
+    });
+
+    importPathCmd = options.importPath;
+
+    var execPath = fs.realpathSync(process.execPath);
+    cmd = template('./node_modules/node-sass/bin/node-sass  ${sassDir} -o ${cssDir} ${importPath}', {
+      sassDir: options.sassDir,
+      cssDir: options.cssDir,
+      importPath: importPathCmd
+    });
+
+    done = this.async();
+    // done(true);
+
+    exec(cmd, function (error, stdout, stderr) {
+      console.log(error);
+      console.log(stdout);
+      console.log(stderr);
+      done(true);
+    });
+  });
+
+};
